7d5be8010fe38bd51489fd04cd9d761eb7e2f7b5
Weronika <weronikapaluch@outlook.com>
2024-06-13 19:35:55 +0200

    changed file name

--- invert_values.py
+++ invert_values.py
@@ -15,3 +15,30 @@ def invert_1(lst):
 
 def ivert_2(lst):
     return [-el for el in lst]
+
+    
+# Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
+
+def disemvowel(string_):
+    vowels = ("e","i","o","u","a")
+    result = [letter for letter in string_ if letter.lower() not in vowels] 
+    string_ = ''.join(result) 
+    return string_
+
+
+# In this Kata we are passing a number (n) into a function.
+# Your code will determine if the number passed is even (or not).
+# The function needs to return either a true or false.
+# Numbers may be positive or negative, integers or floats.
+# Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+def is_even(n): 
+    if n % 2 ==0:
+        return True
+    else:
+        return False
+    pass
