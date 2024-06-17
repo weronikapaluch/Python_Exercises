798f85dc6fb4c85a8b3f6835f78fac5877ea564f
Weronika <weronikapaluch@outlook.com>
2024-06-17 15:30:31 +0200

    added outlier task

--- invert_values.py
+++ invert_values.py
@@ -78,3 +78,25 @@ def find_it(seq):
 
 def create_phone_number(n):
     return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+
+# You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+# Examples
+# [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+# [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+    def find_outlier(integers):
+    lst_even = []
+    lst_odd = []
+    
+    for n in integers:
+        if n % 2 == 0:
+            lst_even.append(n)
+        else:
+            lst_odd.append(n)
+    
+    if len(lst_even) > len(lst_odd):
+        return lst_odd[0]
+    else:
+        return lst_even[0]
