312fcfdd58824bec7e62a39bc1ac711284e90d22
Weronika <weronikapaluch@outlook.com>
2024-06-17 14:57:15 +0200

    added odd amount task

--- invert_values.py
+++ invert_values.py
@@ -42,3 +42,27 @@ def is_even(n):
     else:
         return False
     pass
+
+
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+# Examples
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+def find_it(seq):
+    dic = {}
+    for dig in seq:
+        if dig in dic:
+            dic[dig] += 1
+        else:
+            dic[dig] = 1
+            
+    for key, value in dic.items():
+        if value % 2 != 0:
+            return key
