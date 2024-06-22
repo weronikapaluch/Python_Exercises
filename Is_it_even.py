a4281bb601fe03232d116cd2fafcff47201e3344
Weronika <weronikapaluch@outlook.com>
2024-06-22 17:22:07 +0200

    added high_and_low

--- Is_it_even.py
+++ Is_it_even.py
@@ -15,3 +15,15 @@ def is_even(n):
 # String will never be empty and you do not need to account for different data types.
 def find_short(s):
     return min(len(x) for x in s.split())
+
+
+# In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+# Examples
+# high_and_low("1 2 3 4 5")  # return "5 1"
+
+def high_and_low(numbers):
+    lst_numbers = numbers.split()
+    lst_numbers = [int(n) for n in lst_numbers]
+    max_num = max(lst_numbers)
+    min_num = min(lst_numbers)
+    return f"{max_num} {min_num}"
