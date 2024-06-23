8e478cbe1f0d74c17518fc3c3d7ca7c50edf9d3a
Weronika <weronikapaluch@outlook.com>
2024-06-23 17:33:26 +0200

    added duplicate_count

--- working_on_strings.py
+++ working_on_strings.py
@@ -47,3 +47,22 @@ def only_duplicates(st):
             counter[el] = 1
     
     return ''.join([el for el in st if counter[el] > 1])
+
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+def duplicate_count(text):
+    text = text.lower()
+    dic = {}
+    counter = 0
+    for el in text:
+        if el not in dic:
+            dic[el] = 1
+        elif el in dic:
+            dic[el] += 1
+    
+    for key, value in dic.items():
+        if value > 1:
+            counter += 1
+        
+    return counter
