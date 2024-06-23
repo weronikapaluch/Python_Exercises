8a8a1bcc1cf2e96ba549392e9303682d6ea4b7ca
Weronika <weronikapaluch@outlook.com>
2024-06-23 19:59:46 +0200

    added get_middle

--- working_on_strings.py
+++ working_on_strings.py
@@ -66,3 +66,11 @@ def duplicate_count(text):
             counter += 1
         
     return counter
+
+
+#Get the middle character
+#You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. 
+#If the word's length is even, return the middle 2 characters.
+
+def get_middle(s):
+    return s[(len(s)-1)//2:(len(s)+2)//2]
