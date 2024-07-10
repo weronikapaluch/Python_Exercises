3476e1172e4f4eb7318090d2840c8e9e0e004c5f
Weronika <weronikapaluch@outlook.com>
2024-07-10 21:12:13 +0200

    added between function

--- invert_values.py
+++ invert_values.py
@@ -137,3 +137,9 @@ def positive_sum(arr):
         if num > 0:
             result.append(num)
     return sum(result)
+
+# Complete the function that takes two integers (a, b, where a < b) 
+# and return an array of all integers between the input parameters, including them.
+
+def between(a,b):
+    return list(range(a,b+1))
