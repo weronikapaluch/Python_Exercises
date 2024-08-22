4a27c405921cc8acb93401efe868ead280f982e3
Weronika <weronikapaluch@outlook.com>
2024-08-22 13:25:34 +0200

    add an alternative

--- working_on_strings.py
+++ working_on_strings.py
@@ -257,3 +257,11 @@ def domain_name(url):
         else:
             return domain_parts[-2]
 
+#Alternative 
+
+def domain_name(url):
+    url = url.replace('www.','')
+    url = url.replace('https://','')
+    url = url.replace('http://','')
+    
+    return url[0:url.find('.')]
