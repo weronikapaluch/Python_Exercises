8dd7dfb7dd01aedd4087cf7b27862f2ebb3605a3
Weronika <weronikapaluch@outlook.com>
2024-10-07 21:48:33 +0200

    add a new function

--- Positional_number_systems.py
+++ Positional_number_systems.py
@@ -9,3 +9,22 @@ for el in string:
     print(f"charcter: {el}, ASCII Code: {nr}")
   elif nr > 128:
     print(f"character: {el}, Unicode Code Point: {ord(el):04X}")
+
+
+
+#From a decimal number to a hexadecimal number
+def decimal_to_hexadecimal(decimal_number):
+  hexadecimal_digits = '0123456789ABCDEF'
+  hexadecimal_number = ""
+
+  while decimal_number > 0:
+    remainder = decimal_number % 16
+    hexadecimal_number = hexadecimal_digits[remainder] + hexadecimal_number
+    decimal_number //= 16
+
+  return hexadecimal_number
+
+decimal_number = int(input("Enter a decimal number: "))
+hexadecimal_number = decimal_to_hexadecimal(decimal_number)
+print("The hexadecimal represntation of", decimal_number, "is", hexadecimal_number)
+
